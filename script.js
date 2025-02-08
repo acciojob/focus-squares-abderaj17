b5387e7b0de6e965ad5a62a49986ca56dc184be0
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-02-08 14:54:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const squares = document.querySelectorAll(".square");
+
+for(let i = 1; i<=squares; i++){
+
+	squares.addEventListener('mouseover', ()=>{
+		squares.style.backgroudColor = "#E6E6FA";
+	})
+}
