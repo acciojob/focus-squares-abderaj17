397f2ea08b51d19241ef5113992207a98d330c3e
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-02-08 15:03:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ for(let i = 0; i < squares.length; i++){
 	squares[i].addEventListener('mouseover', ()=>{
 		squares[i].style.backgroundColor = "#6F4E37";
 	})
+}
+
+for(let i = 0; i <squares.length; i++){
 	squares[i].addEventListener('mouseout', ()=>{
-		
+		squares[i].style.backgroundColor = "#E6E6FA";
 	})
 }
